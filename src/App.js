58ea6bef42f2951e7e8db3ea4abb57b4b70bad4e
EnigmaGlimmer <enigmaglimmer0102@outlook.com>
2023-08-05 09:41:45 +0700

    khanh - add homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 
 // App Router Component
 import AppRouter from './router';
