e74ca3b669cefc3a969a35c5010fe64893f8e05f
EnigmaGlimmer <enigmaglimmer0102@outlook.com>
2023-08-08 08:48:26 +0700

    khanh - adđ service

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,7 +1,130 @@
 import React from 'react';
-
+import serviceBanner from '../images/serviceBanner.png';
+import serviceContent from '../images/serviceContent.png';
+import aboutDaisy1 from '../images/aboutDaisy1.png';
+import aboutDaisy2 from '../images/aboutDaisy2.png';
+import aboutDaisy3 from '../images/aboutDaisy3.png';
+import aboutDaisy4 from '../images/aboutDaisy4.png';
+import homeFlowerDeco from '../images/home/flower.svg';
+import aboutBooking from '../images/aboutBooking.png'
+import './style/service.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Pagination } from 'swiper/modules';
+const listImg = [
+    aboutDaisy1,aboutDaisy2,aboutDaisy3,aboutDaisy4,aboutDaisy1,aboutDaisy2,aboutDaisy3,aboutDaisy4
+]
 function Service() {
-    return <div>Service</div>;
+    return <div>
+        {/* Service Banner */}
+        <div className='service-banner py-5'>
+            <div className='service-banner-form'>
+                <img src={serviceBanner} alt='service-banner' width={'100%'}/>
+            </div>
+        </div>
+        {/* Service Name */}
+        <div className='service-name'>
+            <div className='service-name-form'>
+                <div className='service-name-title'>
+                    Service Name
+                </div>
+                <p className='service-name-content'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Eget magna done fermentum iaculis eu non diam phasellus vestibulum. 
+                    Id donec ultrices tincidunt arcu. Malesuada proin libero nunc consequat interdum varius sit amet. 
+                    Eu ultrices vitae auctor eu augue ut lectus. Suscipit tellus mauris a diam. 
+                    Viverra orci sagittis eu volutpat odio facilisis etmagnis dis parturient mananis 
+                    interdum arcu ac tortor dignissim convallis aenean.
+                </p>
+                <div className='service-name-content-list'>
+                    <p className=''>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                    <p className=''>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                    <p className=''>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                </div>
+                <div className='service-name-img-form'>
+                    <img src={serviceContent} width={"100%"}/>
+                    <p className='service-name-img-alt'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    </p>
+                </div>
+                <p className='service-name-content'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Eget magna done fermentum iaculis eu non diam phasellus vestibulum. 
+                    Id donec ultrices tincidunt arcu. Malesuada proin libero nunc consequat interdum varius sit amet. 
+                    Eu ultrices vitae auctor eu augue ut lectus. Suscipit tellus mauris a diam. 
+                    Viverra orci sagittis eu volutpat odio facilisis etmagnis dis parturient mananis 
+                    interdum arcu ac tortor dignissim convallis aenean.
+                </p>
+                <p className='service-name-content'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Eget magna done fermentum iaculis eu non diam phasellus vestibulum. 
+                    Id donec ultrices tincidunt arcu. Malesuada proin libero nunc consequat interdum varius sit amet. 
+                    Eu ultrices vitae auctor eu augue ut lectus. Suscipit tellus mauris a diam. 
+                    Viverra orci sagittis eu volutpat odio facilisis etmagnis dis parturient mananis 
+                    interdum arcu ac tortor dignissim convallis aenean.
+                </p>
+            </div>
+        </div>
+        {/* Daisy Images */}
+        <div className='about-image-store'>
+            <div className='about-image-store-form'>
+                <div className='about-image-store-content'>
+                    <div className='about-image-store-title'>
+                        Little Daisy Stores
+                    </div>
+                    <div className='about-image-store-address'>
+                        ADD: 198 Lorem ipsum Street, MB, AU <span className='about-image-store-btn'>VIEW MAPS</span>
+                    </div>
+                </div>
+                <div className='about-image-store-list'>
+                        <Swiper
+                            slidesPerView={4}
+                            spaceBetween={30}
+                            freeMode={true}
+                            pagination={{
+                            clickable: true,
+                            }}
+                            modules={[FreeMode, Pagination]}
+                            className="about-image-store-swiper"
+                        >
+                            {listImg?.map((item,index) => {
+                                return(
+                                <SwiperSlide key={index}>
+                                    <div className='about-image-store-form-child'>
+                                        <img src={item} width={"100%"}/>
+                                        <p className='about-image-store-form-content'>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                            sed do eiusmod tempor.
+                                        </p>
+                                    </div>
+                                </SwiperSlide>
+                                )
+                            })}
+                        </Swiper>
+                </div>
+            </div>
+        </div>
+        {/* Booking */}
+        <div className='about-booking'>
+            <div className='about-booking-deco booking-f'>
+                <img src={homeFlowerDeco} width={"100%"}/>
+            </div>
+            <div className='about-booking-img'>
+                <img src={aboutBooking} width={"100%"}/>
+            </div>
+            <div className='about-booking-content'>
+                <div className='about-booking-title'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+                </div>
+                <button>Book Now</button>
+            </div>
+        </div>
+    </div>;
 }
 
 export default Service;
