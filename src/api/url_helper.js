724ca262659c48826ddbc0c3fff3e3770815880d
EnigmaGlimmer <enigmaglimmer0102@outlook.com>
2023-08-17 15:16:46 +0700

    tinh - add cloudinary

--- src/api/url_helper.js
+++ src/api/url_helper.js
@@ -7,3 +7,5 @@ export const DELETE_BLOG = '';
 // 2. Comment
 export const GET_COMMENT_LIST = '/api/comment/list';
 export const POST_COMMENT = '/api/comment';
+
+// 3. Upload
--- src/components/upload-modal/upload-modal.js
+++ src/components/upload-modal/upload-modal.js
@@ -1,22 +1,33 @@
 import React, { useRef } from 'react';
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
+import CloudinaryUploadWidget from './upload-widget';
+
+import './upload-modal.css';
 
 function UploadModal({ onCopyLink, show, onHide, onSave, selected, onSelected }) {
     const [uploadedImages, setUploadedImages] = React.useState([
         'https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000',
         'https://www.freecodecamp.org/news/content/images/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg',
+        'https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000',
+        'https://www.freecodecamp.org/news/content/images/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg',
     ]);
     let uploadInputRef = useRef(null);
 
+    React.useEffect(() => {
+        setUploadedImages();
+        return () => {};
+    }, []);
+
     return (
         <div>
-            <Modal show={show} onHide={() => onHide(show)} animation={false}>
+            <Modal size="lg" show={show} onHide={() => onHide(show)} animation={true}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Uploads Modal</Modal.Title>
+                    <Modal.Title>Assets Modal</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Button
                         variant="outline"
+                        className="btn btn-primary my-5"
                         onClick={() => {
                             if (uploadInputRef.current) {
                                 uploadInputRef.current.click();
@@ -35,11 +46,19 @@ function UploadModal({ onCopyLink, show, onHide, onSave, selected, onSelected })
                         }}
                     ></Form.Control>
 
-                    <div className="">
-                        <Row>
-                            {uploadedImages.map((image, index) => {
-                                return (
-                                    <Col className="position-relative" key={index}>
+                    <Row className="" id="asset-list">
+                        {uploadedImages.map((image, index) => {
+                            return (
+                                <Col
+                                    className="position-relative mb-2"
+                                    sm="12"
+                                    md="6"
+                                    lg="4"
+                                    xl="3"
+                                    key={index}
+                                    style={{ height: 'fit-content' }}
+                                >
+                                    <div className="position-relative">
                                         <img className="position-relative" src={image} width="100%"></img>
                                         <div
                                             className="position-absolute top-0 start-0"
@@ -61,20 +80,20 @@ function UploadModal({ onCopyLink, show, onHide, onSave, selected, onSelected })
                                                 Copy this link
                                             </span>
                                         </div>
-                                    </Col>
-                                );
-                            })}
-                        </Row>
-                    </div>
+                                    </div>
+                                </Col>
+                            );
+                        })}
+                    </Row>
                 </Modal.Body>
-                <Modal.Footer>
+                {/* <Modal.Footer>
                     <Button variant="secondary" onClick={() => onHide(show)}>
                         Close
                     </Button>
                     <Button variant="primary" onClick={() => onSave(show)}>
                         Save Changes
                     </Button>
-                </Modal.Footer>
+                </Modal.Footer> */}
             </Modal>
         </div>
     );
